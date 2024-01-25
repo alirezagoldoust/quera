658a9ee429303e526b41ddc5411300e53563258d
Alireza <alirezagoldoos@gmail.com>
2024-01-25 12:18:43 +0330

    Add: Adding models to admin pannel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ProgrammingLanguage, Problem, TeacherQuestion, QGPTQuestion
 
 # Register your models here.
+admin.site.register(ProgrammingLanguage)
+admin.site.register(Problem)
+admin.site.register(TeacherQuestion)
+admin.site.register(QGPTQuestion)
